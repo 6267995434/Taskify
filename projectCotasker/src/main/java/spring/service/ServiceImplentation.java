00bf468d81e17e0f4e8e36fde17b5ddf665585bc
Akshay Dubey <Akshaydubey257@gmail.com>
2025-02-01 23:59:15 +0530

    commited

---
+++ projectCotasker/src/main/java/spring/service/ServiceImplentation.java
@@ -0,0 +1,117 @@
+package spring.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import spring.dto.UserDTORequest;
+import spring.dto.UserDTOResponse;
+import spring.entity.UsersTable;
+import spring.repository.Repo;
+
+@Service
+public class ServiceImplentation implements Services {
+	
+	
+	
+	@Autowired		
+	 Repo repo;
+	
+	@Override
+	public UserDTOResponse addUser(UserDTORequest dto) {
+		
+		UsersTable user = new UsersTable();
+		
+		BeanUtils.copyProperties(dto, user);
+		
+		user.setDeleteflag(false);
+		
+		repo.save(user);
+		
+		UserDTOResponse responsedto  = new UserDTOResponse();
+		
+		responsedto.setId(dto.getId());
+		responsedto.setMessage("Created with UserID: " + user.getId());
+		
+		return responsedto;
+		
+	}
+
+	@Override
+	public boolean signin(String username, String password) {
+		// TODO Auto-generated method stub
+		
+		Optional<UsersTable> userOptional = repo.findByUsername(username);
+        
+        if (userOptional.isPresent()) {
+            UsersTable user = userOptional.get();
+            
+            if(user.isDeleteflag() == true) 
+            {
+            	return false;
+            }
+            
+            return user.getPassword().equals(password); 
+        }
+        
+        return false;
+	}
+
+	@Override
+	public boolean deleteUser(String username, String password) {
+		// TODO Auto-generated method stub
+		
+		Optional<UsersTable> userOptional = repo.findByUsername(username);
+		
+		if (userOptional.isPresent())
+		{
+            UsersTable user = userOptional.get();
+            
+            if(user.getPassword().equals(password))
+            {
+            user.setDeleteflag(true);
+            repo.save(user);
+            return true;
+            }
+        }
+		return false;
+	}
+
+	@Override
+	public UserDTOResponse updatePassword(String username, String password, String newpassword) {
+		// TODO Auto-generated method stub
+		
+
+		Optional<UsersTable> userOptional = repo.findByUsername(username); 
+		if (userOptional.isPresent())
+		{
+            UsersTable user = userOptional.get();
+            
+           if( user.getPassword().equals(password)) {
+        	   user.setPassword(newpassword);
+           }
+           
+           UserDTOResponse responsedto  = new UserDTOResponse();
+       	responsedto.setMessage("Your password has been updated!!");
+           return responsedto;
+		}
+		
+		UserDTOResponse responsedto  = new UserDTOResponse();
+       	responsedto.setMessage("Your password cannot be updated wrong username or password!!");
+		
+		return responsedto;
+	}
+
+	@Override
+	public UserDTOResponse logout() {
+		// TODO Auto-generated method stub
+		
+		UserDTOResponse responsedto  = new UserDTOResponse();
+	responsedto.setMessage("You have been logged out !!");
+		
+		return responsedto;
+	}
+
+}
